363029ed135cc751fdabb486f776bb75d35633d2
premier213 <premierp1213@gmail.com>
2022-02-09 11:19:52 +0330

    fix:remove unused packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,7 @@
-import { AppShell, Header } from '@mantine/core';
-
 export default function HomePage() {
   return (
     <>
-      <AppShell
-        header={
-          <Header height='3.75rem' padding='md'>
-            4
-          </Header>
-        }
-      >
-        <div>444</div>
-      </AppShell>
+      <div>444</div>
     </>
   );
 }
